aef175155e73840904c4592842141e1322729771
KALTURA\Shmulik.Kummer <shmulik.kummer@kaltura.com>
2018-04-17 09:41:21 +0300

    Updated  AppTokenTests class

--- src/test/java/com/kaltura/client/test/tests/servicesTests/appTokenTests/StartSessionTests.java
+++ src/test/java/com/kaltura/client/test/tests/servicesTests/appTokenTests/StartSessionTests.java
@@ -16,6 +16,7 @@ import io.qameta.allure.Description;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import static com.kaltura.client.test.utils.BaseUtils.getAPIExceptionFromList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StartSessionTests extends BaseTest {
@@ -27,7 +28,7 @@ public class StartSessionTests extends BaseTest {
     private AppToken appToken = new AppToken();
     public static Client client;
     private String sessionPrivileges = "key1:value1,key2:value2";
-    private Long expiryDate;
+    private int expiryDate;
     private String anonymousKs;
 
 
@@ -38,15 +39,16 @@ public class StartSessionTests extends BaseTest {
         Response<LoginSession> loginSessionResponse = OttUserServiceImpl.anonymousLogin(client, Properties.PARTNER_ID, udid1);
         anonymousKs = loginSessionResponse.results.getKs();
         client.setKs(operatorKs);
-        expiryDate = BaseUtils.getTimeInEpoch(1);
+        expiryDate = Math.toIntExact(BaseUtils.getTimeInEpoch(1));
     }
 
     @Description("appToken/action/startSession - SHA1")
     @Test
     private void startSessionSha1() {
+        client.setKs(operatorKs);
         hashType = AppTokenHashType.SHA1;
         // Build appToken object
-        appToken = AppTokenUtils.addAppToken(sessionUserId, hashType, sessionPrivileges, Math.toIntExact(expiryDate));
+        appToken = AppTokenUtils.addAppToken(sessionUserId, hashType, sessionPrivileges, expiryDate);
         // Invoke AppToken/action/add
         Response<AppToken> appTokenResponse = AppTokenServiceImpl.add(client, appToken);
         client.setKs(anonymousKs);
@@ -60,14 +62,14 @@ public class StartSessionTests extends BaseTest {
         assertThat(sessionInfoResponse.results.getKs()).isNotEmpty();
         assertThat(sessionInfoResponse.results.getPartnerId()).isEqualTo(Properties.PARTNER_ID);
         assertThat(sessionInfoResponse.results.getUserId()).isEqualTo(sessionUserId);
-        assertThat(sessionInfoResponse.results.getExpiry()).isEqualTo(Math.toIntExact(expiryDate));
+        assertThat(sessionInfoResponse.results.getExpiry()).isEqualTo(expiryDate);
         assertThat(sessionInfoResponse.results.getPrivileges()).contains(sessionPrivileges);
         assertThat(sessionInfoResponse.results.getUdid()).isEqualTo(udid1);
         assertThat(sessionInfoResponse.results.getCreateDate()).isNotZero();
 
         // Invoke AppToken/action/startSession - with udid2
         sessionInfoResponse = AppTokenServiceImpl.startSession(client, appTokenResponse.results.getId()
-                , tokenHash, null, Math.toIntExact(expiryDate), udid2);
+                , tokenHash, null, expiryDate, udid2);
 
         assertThat(sessionInfoResponse.results.getKs()).isNotEmpty();
 
@@ -78,9 +80,10 @@ public class StartSessionTests extends BaseTest {
     @Description("appToken/action/startSession - SHA256")
     @Test
     private void startSessionSha256() {
+        client.setKs(operatorKs);
         hashType = AppTokenHashType.SHA256;
         // Build appToken object
-        appToken = AppTokenUtils.addAppToken(sessionUserId, hashType, sessionPrivileges, Math.toIntExact(expiryDate));
+        appToken = AppTokenUtils.addAppToken(sessionUserId, hashType, sessionPrivileges, expiryDate);
         // Invoke AppToken/action/add
         Response<AppToken> appTokenResponse = AppTokenServiceImpl.add(client, appToken);
         client.setKs(anonymousKs);
@@ -88,22 +91,118 @@ public class StartSessionTests extends BaseTest {
         String tokenHash = AppTokenUtils.getTokenHash(hashType, anonymousKs, appTokenResponse.results.getToken());
         // // Invoke AppToken/action/startSession - with udid1
         Response<SessionInfo> sessionInfoResponse = AppTokenServiceImpl.startSession(client, appTokenResponse.results.getId()
-                , tokenHash, null, Math.toIntExact(expiryDate), udid1);
+                , tokenHash, null, expiryDate, udid1);
 
         assertThat(sessionInfoResponse.results.getKs()).isNotEmpty();
         assertThat(sessionInfoResponse.results.getPartnerId()).isEqualTo(Properties.PARTNER_ID);
         assertThat(sessionInfoResponse.results.getUserId()).isEqualTo(sessionUserId);
         assertThat(sessionInfoResponse.results.getExpiry()).isEqualTo(expiryDate);
-        assertThat(sessionInfoResponse.results.getPrivileges()).isEqualTo(sessionPrivileges);
+        assertThat(sessionInfoResponse.results.getPrivileges()).contains(sessionPrivileges);
         assertThat(sessionInfoResponse.results.getUdid()).isEqualTo(udid1);
         assertThat(sessionInfoResponse.results.getCreateDate()).isNotZero();
 
         // Invoke AppToken/action/startSession - with udid2
         sessionInfoResponse = AppTokenServiceImpl.startSession(client, appTokenResponse.results.getId()
-                , tokenHash, null, Math.toIntExact(expiryDate), udid2);
+                , tokenHash, null, expiryDate, udid2);
 
         assertThat(sessionInfoResponse.results.getKs()).isNotEmpty();
 
+
         // TODO - Add session/action/get request with ks received from startSession API
     }
+
+    @Description("appToken/action/startSession - with invalid token hash")
+    @Test
+    private void startSessionWithInvalidTokenHash() {
+        client.setKs(operatorKs);
+        hashType = AppTokenHashType.SHA256;
+        // Build appToken object
+        appToken = AppTokenUtils.addAppToken(sessionUserId, hashType, sessionPrivileges, expiryDate);
+        // Invoke AppToken/action/add
+        Response<AppToken> appTokenResponse = AppTokenServiceImpl.add(client, appToken);
+        client.setKs(anonymousKs);
+        // Invalid token hash
+        String tokenHash = "1234";
+        // // Invoke AppToken/action/startSession - with invalid token hash
+        Response<SessionInfo> sessionInfoResponse = AppTokenServiceImpl.startSession(client, appTokenResponse.results.getId()
+                , tokenHash, null, expiryDate, udid1);
+
+        assertThat(sessionInfoResponse.error.getCode()).isEqualTo(getAPIExceptionFromList(50022).getCode());
+
+    }
+
+    @Description("appToken/action/startSession - with invalid id")
+    @Test
+    private void startSessionWithInvalidId() {
+        client.setKs(operatorKs);
+        hashType = AppTokenHashType.SHA256;
+        // Build appToken object
+        appToken = AppTokenUtils.addAppToken(sessionUserId, hashType, sessionPrivileges, expiryDate);
+        // Invoke AppToken/action/add
+        Response<AppToken> appTokenResponse = AppTokenServiceImpl.add(client, appToken);
+        client.setKs(anonymousKs);
+        // Invalid token hash
+        String tokenHash = AppTokenUtils.getTokenHash(hashType, anonymousKs, appTokenResponse.results.getToken());
+        // // Invoke AppToken/action/startSession - with invalid token hash
+        Response<SessionInfo> sessionInfoResponse = AppTokenServiceImpl.startSession(client, "1234"
+                , tokenHash, null, expiryDate, udid1);
+
+        assertThat(sessionInfoResponse.error.getCode()).isEqualTo(getAPIExceptionFromList(50055).getCode());
+
+    }
+
+    @Description("appToken/action/startSession - with expired id")
+    @Test
+    private void startSessionWithExpiredId() {
+        client.setKs(operatorKs);
+        hashType = AppTokenHashType.SHA1;
+        // Build appToken object
+        appToken = AppTokenUtils.addAppToken(sessionUserId, hashType, sessionPrivileges, expiryDate);
+        // Invoke AppToken/action/add
+        Response<AppToken> appTokenResponse = AppTokenServiceImpl.add(client, appToken);
+        client.setKs(anonymousKs);
+
+        // Generate new token hash
+        String tokenHash = AppTokenUtils.getTokenHash(hashType, anonymousKs, appTokenResponse.results.getToken());
+
+        // Wait until app token id is expired
+        try {
+            Thread.sleep(72000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Invoke AppToken/action/startSession - verify exception is thrown - app token id expired ("application-token id not found")
+        Response<SessionInfo> sessionInfoResponse = AppTokenServiceImpl.startSession(client, appTokenResponse.results.getId()
+                , tokenHash, null, Math.toIntExact(expiryDate), udid1);
+
+        assertThat(sessionInfoResponse.error.getCode()).isEqualTo(getAPIExceptionFromList(500055).getCode());
+
+    }
+
+    @Description("appToken/action/startSession - with mismatch hash type (hash type in appToken/action/add request is different from the type generated")
+    @Test
+    private void startSessionWithHashTypeMismatch() {
+        client.setKs(operatorKs);
+        hashType = AppTokenHashType.SHA1;
+        // Build appToken object
+        appToken = AppTokenUtils.addAppToken(sessionUserId, hashType, sessionPrivileges, expiryDate);
+        // Invoke AppToken/action/add
+        Response<AppToken> appTokenResponse = AppTokenServiceImpl.add(client, appToken);
+        client.setKs(anonymousKs);
+
+        // Generate new token hash
+        String tokenHash = AppTokenUtils.getTokenHash(AppTokenHashType.SHA256, anonymousKs, appTokenResponse.results.getToken());
+
+
+        // Invoke AppToken/action/startSession - exception is thrown ("Invalid application-token hash")
+        Response<SessionInfo> sessionInfoResponse = AppTokenServiceImpl.startSession(client, appTokenResponse.results.getId()
+                , tokenHash, null, Math.toIntExact(expiryDate), udid1);
+
+        assertThat(sessionInfoResponse.error.getCode()).isEqualTo(getAPIExceptionFromList(50022).getCode());
+    }
+
+
+    // TODO - Add session/action/get request with ks received from startSession API
+
 }
